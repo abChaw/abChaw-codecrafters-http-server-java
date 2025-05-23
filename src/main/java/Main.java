5133d259f1c4129d59070f67b97cc8c45fb52df7
aektaBr <ekta.bhakar7@gmail.com>
2025-05-23 14:18:14 -0400

    send response body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,30 +4,30 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    try(ServerSocket serverSocket = new ServerSocket(4221)){
-    //
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
-      while(true) {
-          Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+        // Uncomment this block to pass the first stage
+        //
+        try (ServerSocket serverSocket = new ServerSocket(4221)) {
+            //
+            //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            //   // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
 
-          String httpResponse = getString(clientSocket);
-          OutputStream out = clientSocket.getOutputStream();
-          out.write(httpResponse.getBytes(StandardCharsets.UTF_8));
-          clientSocket.close();
-          System.out.println("Response sent, connection closed");
-      }
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+                String httpResponse = getString(clientSocket);
+                OutputStream out = clientSocket.getOutputStream();
+                out.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+                clientSocket.close();
+                System.out.println("Response sent, connection closed");
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 
     private static String getString(Socket clientSocket) throws IOException {
         BufferedReader in = new BufferedReader(
@@ -38,13 +38,20 @@ public class Main {
         for (String charStream; (charStream = in.readLine()) != null && !charStream.isEmpty(); ) {
 
         }
-        String httpResponse = "HTTP/1.1 200 OK\r\n";
-        if ("GET".equals(lines[0]) && lines[1].startsWith("/echo")) {
-            String msgBody=lines[1].substring(lines[1].indexOf('o') + 2);
-            String hContentType="Content-Type: text/plain\r\n";
-            String hContentLength="Content-Length:" + msgBody.length()+ "\r\n\r\n";
+        String httpResponse = "HTTP/1.1 404 Not Found\r\n";
+        if ("GET".equals(lines[0])) {
+            if (lines[1].startsWith("/")) {
+                httpResponse = "HTTP/1.1 200 OK\r\n";
+                String msgBody = "";
+                if (lines[1].startsWith("/echo")) {
+                    msgBody = lines[1].substring(lines[1].indexOf('o') + 2);
+                    String hContentType = "Content-Type: text/plain\r\n";
+                    String hContentLength = "Content-Length:" + msgBody.length() + "\r\n\r\n";
 
-            httpResponse += hContentType + hContentLength + msgBody ;
+                    httpResponse += hContentType + hContentLength + msgBody;
+                }
+
+            }
         }
         return httpResponse;
     }
